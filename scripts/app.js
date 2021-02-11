3bd340b762022e88dd49b8fbee95c861a4c2dd61
Debashis Debnath <sunnydebashis2014@gmail.com>
2021-02-11 22:51:47 +0530

    Light theme toggle

--- scripts/app.js
+++ scripts/app.js
@@ -324,3 +324,18 @@ function filefireBase(file,fname){
   var studentPhotos=firebase.storage().ref(`students/students-${fname}`)
   studentPhotos.put(file)
 }
+
+//Theme
+let theme=document.getElementById('theme')
+let themeCheck=document.querySelector('.theme-text')
+document.querySelector('#themeToggele').addEventListener('click',()=>{
+  if(theme.href==="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/darkly/bootstrap.min.css")
+  {
+   theme.href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/superhero/bootstrap.min.css"
+   themeCheck.innerText="Switch to Dark mode"
+  }
+  else{
+    theme.href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/darkly/bootstrap.min.css"
+    themeCheck.innerText="Switch to Light mode"
+  }
+})
